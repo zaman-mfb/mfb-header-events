b90719451c973130392845c5dae196a5c1524e80
zaman-mfb <zaman@muslimfoodbank.com>
2026-01-30 19:52:16 +0500

    Update events.js

--- events/events.js
+++ events/events.js
@@ -1,1 +1,18 @@
+const events = [
+  {
+    image: "https://muslimfoodbank.com/wp-content/uploads/2025/08/Yellow-and-Purple-Modern-Digital-Marketing-Job-Vacancy-LinkedIn-Post-2.png",
+    title: "Virtual Workshops 2026",
+    link: "https://muslimfoodbank.com/virtual-workshops/"
+  },
+  {
+    image: "https://muslimfoodbank.com/wp-content/uploads/2025/06/Website-card-Community-Caseworker-card.jpg",
+    title: "ASPIRE Community Caseworker Training",
+    link: "https://muslimfoodbank.com/events/aspire-community-caseworker-training/"
+  },
+  {
+    image: "https://muslimfoodbank.com/wp-content/uploads/2025/06/Website-card-Community-Caseworker-card.jpg",
+    title: "Pre-Ramadan Fundraising Dinner 2026",
+    link: "https://muslimfoodbank.com/fundraiser/surrey/"
+  }
+];
 
